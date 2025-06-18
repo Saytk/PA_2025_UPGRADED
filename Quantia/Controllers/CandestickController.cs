7eb6fce3ea62149945201312d141eaa1cac20fdb
SilveraDan <51488360+SilveraDan@users.noreply.github.com>
2025-06-18 13:13:56 +0200

    ajout prédiction ia

--- Quantia/Controllers/CandestickController.cs
+++ Quantia/Controllers/CandestickController.cs
@@ -18,7 +18,7 @@ namespace Quantia.Controllers
         [HttpGet("load")]
         public async Task<IActionResult> LoadCandles([FromQuery] string symbol, [FromQuery] string start_date, [FromQuery] string end_date)
         {
-            var apiUrl = $"http://127.0.0.1:8004/load-data?symbol={symbol}&start_date={start_date}&end_date={end_date}";
+            var apiUrl = $"http://127.0.0.1:8003/load-data?symbol={symbol}&start_date={start_date}&end_date={end_date}";
             var response = await _httpClient.GetAsync(apiUrl);
 
             if (!response.IsSuccessStatusCode)
@@ -29,5 +29,24 @@ namespace Quantia.Controllers
             var json = await response.Content.ReadAsStringAsync();
             return Content(json, "application/json");
         }
+
+        [HttpGet("predict")]
+        public async Task<IActionResult> GetPredictions([FromQuery] string symbol)
+        {
+            var url = $"http://127.0.0.1:8003/predict-latest?symbol={symbol}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Failed to retrieve prediction data from backend API.");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            return Content(json, "application/json");
+        }
+
     }
+
+
 }
