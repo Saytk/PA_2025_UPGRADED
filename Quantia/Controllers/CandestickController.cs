c42310693fdded48dfd045dbf849426432083242
SilveraDan <51488360+SilveraDan@users.noreply.github.com>
2025-07-08 16:52:42 +0200

    ajout filtre heure et minute

--- Quantia/Controllers/CandestickController.cs
+++ Quantia/Controllers/CandestickController.cs
@@ -18,7 +18,7 @@ namespace Quantia.Controllers
         [HttpGet("load")]
         public async Task<IActionResult> LoadCandles([FromQuery] string symbol, [FromQuery] string start_date, [FromQuery] string end_date)
         {
-            var apiUrl = $"http://127.0.0.1:8003/load-data?symbol={symbol}&start_date={start_date}&end_date={end_date}";
+            var apiUrl = $"http://127.0.0.1:8006/load-data?symbol={symbol}&start_date={start_date}&end_date={end_date}";
             var response = await _httpClient.GetAsync(apiUrl);
 
             if (!response.IsSuccessStatusCode)
@@ -33,7 +33,7 @@ namespace Quantia.Controllers
         [HttpGet("predict")]
         public async Task<IActionResult> GetPredictions([FromQuery] string symbol)
         {
-            var url = $"http://127.0.0.1:8003/predict-latest?symbol={symbol}";
+            var url = $"http://127.0.0.1:8006/predict-latest?symbol={symbol}";
 
             var response = await _httpClient.GetAsync(url);
 
